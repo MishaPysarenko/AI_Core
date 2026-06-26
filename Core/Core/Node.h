a08e8d2c9c423f1dda89fba4b0b5ff6270c686c1
666mda <nikitapastnikita@gmail.com>
2026-06-26 21:09:07 +0300

    Up Date File(s)

---
+++ Core/Core/Node.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<string>
+#include<vector>
+#include<memory>
+struct TYPE
+{
+	void* pData;
+	size_t sizeData;
+};
+
+struct Node
+{
+	TYPE value;
+	double possibility;
+	std::vector<std::shared_ptr<Node>> nearestNodes;
+
+};
